c25428f19c08409dfe4a3ca2c2df75edcccaf951
sunny21rana <sunny21rana@gmail.com>
2026-08-13 01:48:51 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,36 +2,3 @@ plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
-
-android {
-    namespace = "com.aaknav.funding"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.aaknav.funding"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    packaging {
-        resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.navigation:navigation-compose:2.8.8")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-}
